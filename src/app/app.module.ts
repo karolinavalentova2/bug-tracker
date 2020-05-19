4e5f7ac207064e9834bbced5b23289e1fa4c92b6
karolinakv <karolinavalentova2@gmail.com>
2020-05-20 01:54:29 +0200

    added notification at navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './components/root-view/app.component';
 import {Imports} from './app.imports';
 import {Components} from './app.components';
+import { MyProjectsComponent } from './components/views/projects/my-projects/my-projects.component';
 
 @NgModule({
   declarations: [
     ...Components,
+    MyProjectsComponent,
   ],
   imports: [
     ...Imports,
