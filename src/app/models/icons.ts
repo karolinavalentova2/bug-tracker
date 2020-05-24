c297c3788c9c05277140efa816bddcb257003426
karolinakv <karolinavalentova2@gmail.com>
2020-05-25 00:54:46 +0200

    Deployment fix

--- src/app/models/icons.ts
+++ src/app/models/icons.ts
@@ -1,12 +1,12 @@
 export const IconsURIs = {
   // navigation
-  arrowIcon: '/assets/iconsSVG/navigation.svg#arrowIcon',
-  '/projects': '/assets/iconsSVG/navigation.svg#projectsIcon',
-  '/users': '/assets/iconsSVG/navigation.svg#usersIcon',
-  '/settings': '/assets/iconsSVG/navigation.svg#settingsIcon',
-  '/inbox': '/assets/iconsSVG/navigation.svg#inboxIcon',
+  arrowIcon: './assets/iconsSVG/navigation.svg#arrowIcon',
+  '/projects': './assets/iconsSVG/navigation.svg#projectsIcon',
+  '/users': './assets/iconsSVG/navigation.svg#usersIcon',
+  '/settings': './assets/iconsSVG/navigation.svg#settingsIcon',
+  '/inbox': './assets/iconsSVG/navigation.svg#inboxIcon',
 
   //  top bar
-  userProfileIcon: '/assets/iconsSVG/top-bar.svg#userProfileIcon',
-  signOutIcon: '/assets/iconsSVG/top-bar.svg#signOutIcon',
+  userProfileIcon: './assets/iconsSVG/top-bar.svg#userProfileIcon',
+  signOutIcon: './assets/iconsSVG/top-bar.svg#signOutIcon',
 };
